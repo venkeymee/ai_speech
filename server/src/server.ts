d9189823beacf57c1e24c75edb1a35342e303c0e
venkey <venkeymee@gmail.com>
2021-10-04 21:42:31 +0400

    add certs

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,8 @@ import Logger from './config/winstonlogger';
 import { DataBaseConfig } from './config/dbconfig';
 import Container from 'typedi';
 import audioConvert from './controller/audioConvert';
- import userController from './controller/usercontroller';
+import userController from './controller/usercontroller';
+import https from "https";
 
 import fileUpload from 'express-fileupload';
 dotenv.config();
@@ -41,10 +42,10 @@ app.use('/audio', audioConvert);
 app.use('/user', userController);
 
 
-
-
-
-app.listen(PORT, async () => {
+https.createServer({
+  key: fs.readFileSync(path.join(__dirname,'../../cert/server.key')),
+  cert: fs.readFileSync(path.join(__dirname,'../../cert/server.cert'))
+}, app).listen(PORT, async () => {
       await dbConnection.checkConnection(); 
       Logger.info(chalk.yellow(`⚡️ [server]: Server is running at ${chalk.blue(PORT)}`));
   });
